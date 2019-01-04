b67ab34a8216f531a1b4d799aa2cd75139bceec9
Ryan Webb <ryan@ryanwebb.com>
2019-01-04 16:51:52 +0000

    Removed old rotation function

--- Android/app/src/main/java/com/secondfury/nativetoolkit/MainActivity.java
+++ Android/app/src/main/java/com/secondfury/nativetoolkit/MainActivity.java
@@ -217,51 +217,6 @@ public class MainActivity extends Activity {
 	    return mediaFile;
 	}
 	
-	/*public static Bitmap rotateBitmap(Bitmap bitmap, int orientation) 
-	{
-	    Matrix matrix = new Matrix();
-	    switch (orientation) 
-	    {
-	        case ExifInterface.ORIENTATION_NORMAL:
-	            return bitmap;
-	        case ExifInterface.ORIENTATION_FLIP_HORIZONTAL:
-	            matrix.setScale(-1, 1);
-	            break;
-	        case ExifInterface.ORIENTATION_ROTATE_180:
-	            matrix.setRotate(180);
-	            break;
-	        case ExifInterface.ORIENTATION_FLIP_VERTICAL:
-	            matrix.setRotate(180);
-	            matrix.postScale(-1, 1);
-	            break;
-	        case ExifInterface.ORIENTATION_TRANSPOSE:
-	            matrix.setRotate(90);
-	            matrix.postScale(-1, 1);
-	            break;
-	       case ExifInterface.ORIENTATION_ROTATE_90:
-	           matrix.setRotate(90);
-	           break;
-	       case ExifInterface.ORIENTATION_TRANSVERSE:
-	           matrix.setRotate(-90);
-	           matrix.postScale(-1, 1);
-	           break;
-	       case ExifInterface.ORIENTATION_ROTATE_270:
-	           matrix.setRotate(-90);
-	           break;
-	       default:
-	           return bitmap;
-	    }
-	    try {
-	        Bitmap bmRotated = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
-	        bitmap.recycle();
-	        return bmRotated;
-	    }
-	    catch (OutOfMemoryError e) {
-	        e.printStackTrace();
-	        return null;
-	    }
-	}*/
-	
 	@Override
 	protected void onSaveInstanceState(Bundle outState) 
 	{
